365f2545db7452bf463f86bbc7fd771ee4c6ecea
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 16:25:39 +0200

    filter distribution by quantity and money value

--- src/main/java/com/codurance/katalyst/DistributionOfMoney.java
+++ src/main/java/com/codurance/katalyst/DistributionOfMoney.java
@@ -8,8 +8,8 @@ public class DistributionOfMoney {
     public DistributionOfMoney(List<Money> money) {
         this.money = money;
     }
-    public boolean contains(int i, MoneyValue twoHundred) {
-        return false;
+    public boolean contains(int quantity, MoneyValue value) {
+        return money.stream().filter(money -> money.equals(value) && money.quantity() >= quantity).findAny().isPresent();
     }
     
 }
