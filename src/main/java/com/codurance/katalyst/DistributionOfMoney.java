9a110ba743e9dffd40d83479a06502a77560a60c
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 16:08:24 +0200

    create test: contains enought money

---
+++ src/main/java/com/codurance/katalyst/DistributionOfMoney.java
@@ -0,0 +1,15 @@
+package com.codurance.katalyst;
+
+import java.util.List;
+
+public class DistributionOfMoney {
+
+    private List<Money> money;
+    public DistributionOfMoney(List<Money> money) {
+        this.money = money;
+    }
+    public boolean contains(int i, MoneyValue twoHundred) {
+        return false;
+    }
+    
+}
---
+++ src/test/java/com/codurance/katalyst/DistributionOfMoneyTest.java
@@ -0,0 +1,37 @@
+package com.codurance.katalyst;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DistributionOfMoneyTest {
+    
+    private DistributionOfMoney distribution;
+
+    @BeforeEach
+    public void setUp(){
+        List<Money> money = new ArrayList<>(){
+            {
+               add(Money.create(2, MoneyValue.FIVE_HUNDRED));
+               add(Money.create(3, MoneyValue.TWO_HUNDRED));
+               add(Money.create(5, MoneyValue.ONE_HUNDRED));
+               add(Money.create(12, MoneyValue.FIFTY));
+               add(Money.create(20, MoneyValue.TWENTY));
+               add(Money.create(50, MoneyValue.TEN));
+               add(Money.create(100, MoneyValue.FIVE));
+               add(Money.create(250, MoneyValue.TWO));
+               add(Money.create(500, MoneyValue.ONE));
+            }
+        };
+        distribution = new DistributionOfMoney(money);
+    }
+
+    @Test
+    public void contains_enough_money(){
+        assertTrue(this.distribution.contains(2, MoneyValue.TWO_HUNDRED));
+    }
+}
