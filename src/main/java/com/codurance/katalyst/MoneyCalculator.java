a62f318be40fee34c1ced9e8c46531b99cd9b2f6
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 08:36:48 +0200

    for each all money values and compare with quantity

--- src/main/java/com/codurance/katalyst/MoneyCalculator.java
+++ src/main/java/com/codurance/katalyst/MoneyCalculator.java
@@ -1,18 +1,18 @@
 package com.codurance.katalyst;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MoneyCalculator {
     
     List<Money> breakdown(int quantity){
         ArrayList<Money> breakdown = new ArrayList<Money>();
-        if(quantity == 1){
-            breakdown.add(Money.create(1, MoneyValue.ONE));
-        }
-        if(quantity == 2){
-            breakdown.add(Money.create(1, MoneyValue.TWO));
-        }
+        Arrays.asList(MoneyValue.values()).forEach(money ->{
+            if(money.amount() == quantity){
+                breakdown.add(Money.create(1, money));
+            }
+        });
         return breakdown;
     }
 }
