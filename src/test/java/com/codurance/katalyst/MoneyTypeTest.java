946024ca1f6ff7b61ac93bdc79a71e2b0b96bd49
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 07:29:50 +0200

    add asserts when type of money is COIN

--- src/test/java/com/codurance/katalyst/MoneyTypeTest.java
+++ src/test/java/com/codurance/katalyst/MoneyTypeTest.java
@@ -11,10 +11,12 @@ public class MoneyTypeTest {
     @Test
     public void get_type_money_when_quantity_is_one(){
         assertEquals("bill", MoneyType.BILL.by(1));
+        assertEquals("coin", MoneyType.COIN.by(1));
     }
 
     @Test
     public void get_type_money_when_quantity_is_greater_that_one(){
         assertEquals("bills", MoneyType.BILL.by(new Random().nextInt() + 2));
+        assertEquals("coins", MoneyType.COIN.by(new Random().nextInt() + 2));
     }
 }
