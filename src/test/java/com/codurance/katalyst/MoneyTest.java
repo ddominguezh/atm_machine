e70b0ef6dd93ea3efd01bf234d6a70357c11cb4f
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 16:24:14 +0200

    add equals test

--- src/test/java/com/codurance/katalyst/MoneyTest.java
+++ src/test/java/com/codurance/katalyst/MoneyTest.java
@@ -1,6 +1,7 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,10 @@ public class MoneyTest {
     public void print_money_value(){
         Money money = Money.create(2, MoneyValue.FIVE_HUNDRED);
         assertEquals("2 bills of 500.", money.toString());
+    }
+
+    @Test
+    public void money_equals_to_value(){
+        assertTrue(Money.create(1, MoneyValue.ONE).equals(MoneyValue.ONE));
     }
 }
