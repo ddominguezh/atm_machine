878678926cbe7b4156cfc40bd12a2a25d0a85f0b
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 08:28:24 +0200

    return MoneyValue.ONE when quantity is one

--- src/main/java/com/codurance/katalyst/MoneyCalculator.java
+++ src/main/java/com/codurance/katalyst/MoneyCalculator.java
@@ -1,10 +1,15 @@
 package com.codurance.katalyst;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MoneyCalculator {
     
     List<Money> breakdown(int quantity){
-        return null;
+        return new ArrayList<Money>(){
+            {
+                add(Money.create(1, MoneyValue.ONE));
+            }
+        };
     }
 }
