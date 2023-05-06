016b9805759f65d6d0694e58bd8b5d93957bccff
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 07:54:27 +0200

    configure all values of money

--- src/main/java/com/codurance/katalyst/MoneyValue.java
+++ src/main/java/com/codurance/katalyst/MoneyValue.java
@@ -1,7 +1,15 @@
 package com.codurance.katalyst;
 
 public enum MoneyValue {
-    FIVE_HUNDRED(500, MoneyType.BILL);
+    FIVE_HUNDRED(500, MoneyType.BILL),
+    TWO_HUNDRED(200, MoneyType.BILL),
+    ONE_HUNDRED(100, MoneyType.BILL),
+    FIFTY(50, MoneyType.BILL),
+    TWENTY(20, MoneyType.BILL),
+    TEN(10, MoneyType.BILL),
+    FIVE(5, MoneyType.BILL),
+    TWO(2, MoneyType.COIN),
+    ONE(1, MoneyType.COIN);
     private int amount;
     private MoneyType type;
     MoneyValue(int amount, MoneyType type){
--- src/test/java/com/codurance/katalyst/MoneyValueTest.java
+++ src/test/java/com/codurance/katalyst/MoneyValueTest.java
@@ -12,4 +12,60 @@ public class MoneyValueTest {
         assertEquals("1 bill of 500", MoneyValue.FIVE_HUNDRED.toString(1));
         assertEquals("2 bills of 500", MoneyValue.FIVE_HUNDRED.toString(2));
     }
+
+    @Test
+    public void configure_two_hundred_bill(){
+        assertEquals(200, MoneyValue.TWO_HUNDRED.amount());
+        assertEquals("1 bill of 200", MoneyValue.TWO_HUNDRED.toString(1));
+        assertEquals("2 bills of 200", MoneyValue.TWO_HUNDRED.toString(2));
+    }
+
+    @Test
+    public void configure_one_hundred_bill(){
+        assertEquals(100, MoneyValue.ONE_HUNDRED.amount());
+        assertEquals("1 bill of 100", MoneyValue.ONE_HUNDRED.toString(1));
+        assertEquals("2 bills of 100", MoneyValue.ONE_HUNDRED.toString(2));
+    }
+
+    @Test
+    public void configure_fifty_bill(){
+        assertEquals(50, MoneyValue.FIFTY.amount());
+        assertEquals("1 bill of 50", MoneyValue.FIFTY.toString(1));
+        assertEquals("2 bills of 50", MoneyValue.FIFTY.toString(2));
+    }
+
+    @Test
+    public void configure_twenty_bill(){
+        assertEquals(20, MoneyValue.TWENTY.amount());
+        assertEquals("1 bill of 20", MoneyValue.TWENTY.toString(1));
+        assertEquals("2 bills of 20", MoneyValue.TWENTY.toString(2));
+    }
+
+    @Test
+    public void configure_ten_bill(){
+        assertEquals(10, MoneyValue.TEN.amount());
+        assertEquals("1 bill of 10", MoneyValue.TEN.toString(1));
+        assertEquals("2 bills of 10", MoneyValue.TEN.toString(2));
+    }
+
+    @Test
+    public void configure_five_bill(){
+        assertEquals(5, MoneyValue.FIVE.amount());
+        assertEquals("1 bill of 5", MoneyValue.FIVE.toString(1));
+        assertEquals("2 bills of 5", MoneyValue.FIVE.toString(2));
+    }
+
+    @Test
+    public void configure_two_coin(){
+        assertEquals(2, MoneyValue.TWO.amount());
+        assertEquals("1 coin of 2", MoneyValue.TWO.toString(1));
+        assertEquals("2 coins of 2", MoneyValue.TWO.toString(2));
+    }
+
+    @Test
+    public void configure_one_coin(){
+        assertEquals(1, MoneyValue.ONE.amount());
+        assertEquals("1 coin of 1", MoneyValue.ONE.toString(1));
+        assertEquals("2 coins of 1", MoneyValue.ONE.toString(2));
+    }
 }
