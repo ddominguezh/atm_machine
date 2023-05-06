d2bd6ca0996c95caafaaca57c483f4554be480d2
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 08:48:03 +0200

    create test get breakdown when quantity is three

--- src/test/java/com/codurance/katalyst/MoneyCalculatorTest.java
+++ src/test/java/com/codurance/katalyst/MoneyCalculatorTest.java
@@ -35,4 +35,12 @@ public class MoneyCalculatorTest {
         assertEquals(1, breakdown.size());
         assertEquals("2 coins of 2", breakdown.get(0).toString());
     }
+
+    @Test
+    public void get_breakdown_when_quantity_is_three(){
+        List<Money> breakdown = new MoneyCalculator(3).breakdown();
+        assertEquals(2, breakdown.size());
+        assertEquals("1 coin of 2", breakdown.get(0).toString());
+        assertEquals("1 coin of 1", breakdown.get(1).toString());
+    }
 }
