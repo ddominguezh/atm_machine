20de99c617e25013fcecd2806e74edefadf7e634
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-06 08:26:16 +0200

    create test get breakdown when quantity is one

--- src/test/java/com/codurance/katalyst/MoneyCalculatorTest.java
+++ src/test/java/com/codurance/katalyst/MoneyCalculatorTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.Test;
 public class MoneyCalculatorTest {
     
     @Test
-    public void get_money_when_quantity_is_one(){
-        List<Money> moneys = new MoneyCalculator().breakdown(1);
-        assertEquals(1, moneys.size());
-        assertEquals("1 coin of 1", moneys.get(0).toString());
+    public void get_breakdown_when_quantity_is_one(){
+        List<Money> breakdown = new MoneyCalculator().breakdown(1);
+        assertEquals(1, breakdown.size());
+        assertEquals("1 coin of 1", breakdown.get(0).toString());
     }
 }
